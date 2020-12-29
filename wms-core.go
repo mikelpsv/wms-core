8048657410f4a75b5f236dd5a08ef20c0bf1ec2f
Mike <mikelpsv@gmail.com>
2020-12-29 16:32:15 +0300

    v1.0.2

--- wms-core.go
+++ wms-core.go
@@ -1,7 +1,15 @@
 package wms_core
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/mikelpsv/wms-core/models"
+)
 
 func Version() {
 	fmt.Println("Version 1.0.0")
+}
+
+func GetStorage() *models.Storage {
+	storage := new(models.Storage)
+	return storage
 }
